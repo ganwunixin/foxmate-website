0f71a96121fff9024f3ac46686d7f201fa72d2ef
ganwunixin <ganwunixin@github.com>
2026-03-19 11:38:40 +0800

    Update to v1.3.0

--- lib/config.ts
+++ lib/config.ts
@@ -5,13 +5,10 @@ export const config = {
     url: 'https://foxmate2.sfo3.cdn.digitaloceanspaces.com/FoxMateAI_v1.3.0.exe',
     mac: '',
     fileSize: '317 MB',
-    releaseDate: '2026-03-18',
+    releaseDate: '2026-03-19',
     releaseNotes: [
-      '新增用户地理位置追踪',
-      '新增用户来源追踪',
-      '优化IP定位服务（三重备用）',
+      '优化用户体验',
       '增强数据分析能力',
-      '位置缓存24小时自动刷新',
     ],
   },
   app: {
